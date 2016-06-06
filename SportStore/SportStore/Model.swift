049e9c156d6a985efffc514a27133a7d8fcbe96d
Leonardo Lobato <leonardo.lobato@gmail.com>
2016-06-06 17:34:34 -0300

    Basic Layout Done

---
+++ SportStore/SportStore/Model.swift
@@ -0,0 +1,15 @@
+
+import Foundation
+
+class Model{
+    static var products = [
+        (title:"Kayak", description: "A boat for one person", category:"Watersports", price:275.0, quantity:10),
+        (title:"Lifejacket",description: "Protective and fashionable",category: "Watersports",price: 48.95, quantity:14),
+        (title:"Soccer Ball", description:"FIFA-approved size and weight",category: "Soccer", price:19.5, quantity:32),
+        (title:"Corner Flags", description:"Give your playing field a professional touch",category: "Soccer", price:34.95, quantity:1),
+        (title:"Stadium", description:"Flat-packed 35,000-seat stadium",category: "Soccer", price:79500.0, quantity:4),
+        (title:"Thinking Cap", description:"Improve your brain efficiency by 75%", category:"Chess", price:16.0, quantity:8),
+        (title:"Unsteady Chair", description:"Secretly give your opponent a disadvantage",category: "Chess", price:29.95, quantity:3),
+        (title:"Human Chess Board",description: "A fun game for the family", category:"Chess", price:75.0, quantity:2),
+        (title:"Bling-Bling King", description:"Gold-plated, diamond-studded King", category:"Chess", price:1200.0, quantity:4)]
+}
--- SportStore/SportStore/ViewController.swift
+++ SportStore/SportStore/ViewController.swift
@@ -8,16 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProductTableCell: UITableViewCell {
+    @IBOutlet weak var nameLabel:UILabel!
+    @IBOutlet weak var descriptionLabel:UILabel!
+    @IBOutlet weak var stockStepper:UIStepper!
+    @IBOutlet weak var stockField:UITextField!
+    
+    
+}
 
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var totalStockLabel:UILabel!
+    @IBOutlet weak var tableView:UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        displayStockTotal()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.products.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "ProductCell";
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+                    as! ProductTableCell
+        cell.nameLabel.text = Model.products[indexPath.row].title
+        cell.descriptionLabel.text = Model.products[indexPath.row].description
+        cell.stockStepper.value = Double(Model.products[indexPath.row].quantity)
+        cell.stockField.text = String(Model.products[indexPath.row].price)
+        
+        return cell
+    }
+    
+    
+    func displayStockTotal() {
+        let stockTotal = Model.products.reduce(0,combine: {
+            (total,product) -> Int in
+                return total  + product.quantity
+        })
+        totalStockLabel.text = "\(stockTotal) Products in Stock"
+        
     }
 
 
